6913271d319c9ad15a6d8d1eabec73868645dcb5
agoncecelia <agonn.c@gmail.com>
2017-08-20 21:37:45 +0200

    lab exercise comments removed

--- cart.js
+++ cart.js
@@ -28,16 +28,6 @@ function CartDAO(database) {
     this.getCart = function(userId, callback) {
         "use strict";
 
-        /*
-        * TODO-lab5
-        *
-        * LAB #5: Implement the getCart() method.
-        *
-        * Query the "cart" collection by userId and pass the cart to the
-        * callback function.
-        *
-        */
-
         var userCart = {
             userId: userId,
             items: []
@@ -50,11 +40,6 @@ function CartDAO(database) {
             }
             callback(userCart)
         })
-        // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
     }
 
 
@@ -74,55 +59,9 @@ function CartDAO(database) {
                 callback(item)
             }
         })
-        
-        /*
-         *
-         * TODO-lab6
-         *
-         * LAB: #6
-         *
-         * Write a query that will determine whether or not the cart associated
-         * with the userId contains an item identified by itemId. If the cart
-         * does contain the item, pass the item to the callback. If it does not,
-         * pass the value null to the callback.
-         *
-         * NOTE: You should pass only the matching item to the callback. Do not
-         * pass an array of one or more items or the entire cart.
-         *
-         * SUGGESTION: While it is not necessary, you might find it easier to
-         * use the $ operator in a projection document in your call to find() as
-         * a means of selecting the matching item. Again, take care to pass only
-         * the matching item (not an array) to the callback. See:
-         * https://docs.mongodb.org/manual/reference/operator/projection/positional/
-         *
-         * As context for this method to better understand its purpose, look at
-         * how cart.itemInCart is used in the mongomart.js app.
-         *
-         */
 
-
-        // TODO-lab6 Replace all code above (in this method).
     }
 
-
-    /*
-     * This solution is provide as an example to you of several query
-     * language features that are valuable in update operations.
-     * This method adds the item document passed in the item parameter to the
-     * user's cart. Note that this solution works regardless of whether the
-     * cart already contains items or is empty. addItem will be called only
-     * if the cart does not already contain the item. The route handler:
-     * router.post("/user/:userId/cart/items/:itemId"...
-     * handles this. Please review how that method works to have a complete
-     * understanding of how addItem is used.
-     *
-     * NOTE: One may use either updateOne() or findOneAndUpdate() to
-     * write this method. We did not discuss findOneAndUpdate() in class,
-     * but it provides a very straightforward way of solving this problem.
-     * See the following for documentation:
-     * http://mongodb.github.io/node-mongodb-native/2.0/api/Collection.html#findOneAndUpdate
-     *
-     */
     this.addItem = function(userId, item, callback) {
         "use strict";
 
@@ -151,22 +90,6 @@ function CartDAO(database) {
                 callback(result.value);
             });
 
-        /*
-
-          Without all the comments this code looks written as follows.
-
-        this.db.collection("cart").findOneAndUpdate(
-            {userId: userId},
-            {"$push": {items: item}},
-            {
-                upsert: true,
-                returnOriginal: false
-            },
-            function(err, result) {
-                assert.equal(null, err);
-                callback(result.value);
-            });
-        */
 
     };
 
@@ -207,26 +130,6 @@ function CartDAO(database) {
                 }
             )
         }
-        /*
-        * TODO-lab7
-        *
-        * LAB #7: Update the quantity of an item in the user's cart in the
-        * database by setting quantity to the value passed in the quantity
-        * parameter. If the value passed for quantity is 0, remove the item
-        * from the user's cart stored in the database.
-        *
-        * Pass the updated user's cart to the callback.
-        *
-        * NOTE: Use the solution for addItem as a guide to your solution for
-        * this problem. There are several ways to solve this. By far, the
-        * easiest is to use the $ operator. See:
-        * https://docs.mongodb.org/manual/reference/operator/update/positional/
-        *
-        */
-
-        
-
-        // TODO-lab7 Replace all code above (in this method).
 
     }
 
